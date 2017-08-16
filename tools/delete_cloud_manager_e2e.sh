f839cae5e54c629feefdaaa252ee06ee612fd66e
luolibin <luolibin@pingcap.com>
2017-08-16 18:05:53 +0800

    add script to clean test env when e2e test failed

---
+++ tools/delete_cloud_manager_e2e.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cluster=${1:-tidb-cloud-manager-e2e}
+docker exec kube-master curl -X DELETE --header 'Accept: application/json' http://127.0.0.1:32333/pingcap.com/api/v1/clusters/$cluster

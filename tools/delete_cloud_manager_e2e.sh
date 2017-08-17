0e671e7d5da9a92883779ebaf9a50e83482556c0
luolibin <luolibin@pingcap.com>
2017-08-17 13:57:12 +0800

    clean cloud manager e2e job when run failed

--- tools/delete_cloud_manager_e2e.sh
+++ tools/delete_cloud_manager_e2e.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+kubectl delete po/tidb-cloud-manager-e2e -n kube-system
 cluster=${1:-tidb-cloud-manager-e2e}
 docker exec kube-master curl -X DELETE --header 'Accept: application/json' http://127.0.0.1:32333/pingcap.com/api/v1/clusters/$cluster

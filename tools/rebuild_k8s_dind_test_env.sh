af9137e4a118f2952afbd256ae28b1109db610c9
luolibin <luolibin@pingcap.com>
2017-11-23 02:07:38 +0800

    make nginx reverse proxy to correct docker registry in cluster of different namespaces

--- tools/rebuild_k8s_dind_test_env.sh
+++ tools/rebuild_k8s_dind_test_env.sh
@@ -19,35 +19,14 @@ WORKSPACE=$(cd $(dirname $0)/..; pwd)
 cd $WORKSPACE
 
 function rebuild::step {
-   local GREEN="\033[0;32m"
-   local NC="\033[0m"
-   echo -e  ">>> ${GREEN} $@ ${NC}"
-}
-
-function rebuild::clean_dind {
-    rebuild::step "start to cleaning k8s dind cluster with namespace $1"
-    export DIND_NAMESPACE=$1
-    local KUBE_VERSION
-    KUBE_VERSION=`echo ${DIND_NAMESPACE}|awk -F- '{print $NF}'`
-    ./fixed/dind-cluster-${KUBE_VERSION}.sh clean
+   local green="\033[0;32m"
+   local nc="\033[0m"
+   echo -e  ">>> ${green} $@ ${nc}"
 }
 
 function rebuild::clean_images {
-    rebuild::step "start to clean useless images"
+    rebuild::step "start to remove useless images"
     docker images|docker images|grep -v kubeadm-dind-cluster|egrep "tidb-operator|<none>|tidb-cloud-manager"|awk '{print $3}'|xargs -I{} -n1 docker rmi -f {} || true
-}
-
-# the input args: $namespace $dind_subnet $apiserver_port $local_registry_port $cloud_manager_port
-function rebuild::up_dind {
-    rebuild::step "start to bringing up k8s dind cluster with namespace $1"
-    export DIND_NAMESPACE=$1
-    export DIND_SUBNET=$2
-    export APISERVER_PORT=$3
-    export REGISTRY_PORT=$4
-    export CLOUD_MANAGER_PORT=$5
-    local KUBE_VERSION
-    KUBE_VERSION=`echo ${DIND_NAMESPACE}|awk -F- '{print $NF}'`
-    ./fixed/dind-cluster-${KUBE_VERSION}.sh up
 }
 
 function rebuild::start_registry {
@@ -55,35 +34,63 @@ function rebuild::start_registry {
     docker exec $1-kube-master docker run -d --restart=always -v /registry:/var/lib/registry -p5001:5000 --name=registry uhub.ucloud.cn/pingcap/registry:2
 }
 
+# the input args: $namespace $field_index
+function rebuild::get_info_by_ns {
+    local -a results
+    local -a cluster_array_info
+    for cluster in ${CLUSTERS[@]}
+    do
+        cluster_array_info=(`echo ${cluster}|awk -F: '{print $1,$2,$3,$4,$5}'`)
+        if [[ ${#cluster_array_info[@]} -ne 5 ]]
+        then
+            echo "Wrong cluster info format: ${cluster}, please check it!!!" 1>&2
+            exit 1
+        fi
+        if [[ $1 == all ]]
+        then
+            ## collect field infos of all namespace
+            results+=(${cluster_array_info[$1]})
+        else
+            [[ $1 != ${cluster_array_info[0]} ]] && continue
+            results+=(${cluster_array_info[$2]})
+            break
+        fi
+    done
+    if [[ ${#results[@]} -eq 0 ]]
+    then
+        echo "Did not find a matching namespace: $1" 1>&2
+        exit 1
+    fi
+    echo ${results[@]}
+}
+
 function rebuild::deploy_apps {
     rebuild::step "start to deploy [${INIT_DEPLOYS}] to k8s dind cluster with namespace $1"
+    local dind_subnet
+    local master_ip
+
+    dind_subnet=$(rebuild::get_info_by_ns $1 1)
+    master_ip=$(echo ${dind_subnet}|cut -d. -f1-3).2
     for deploy in ${INIT_DEPLOYS}
     do
-       ./tools/clusters/kubectl.$1 create -f ./manifests/${deploy}
+        if [[ ${deploy} == "registry-proxy.yaml" ]]
+        then
+            sed "s/10.192.0.2/${master_ip}/g" ./manifests/${deploy}|./tools/clusters/kubectl.$1 apply -f -
+            continue
+        fi
+        ./tools/clusters/kubectl.$1 apply -f ${deploy}
     done
 }
 
 function rebuild::push_images_to_local {
     local -a cluster_array_info
-    local LOCAL_REGISTRY_PORT
-    local LOCAL_REGISTRY
-    for cluster in ${CLUSTERS[@]}
-    do
-        cluster_array_info=(`echo ${cluster}|awk -F: '{print $1,$2,$3,$4,$5}'`)
-        [[ ${#cluster_array_info[@]} -ne 5 ]] && echo "Wrong cluster info format: ${cluster}, please check it!!!" 1>&2
+    local local_registry_port
+    local local_registry
 
-        [[ $1 != ${cluster_array_info[0]} ]] && continue
-        LOCAL_REGISTRY_PORT=${cluster_array_info[3]}
-        break
-    done
-    if [[ ! ${LOCAL_REGISTRY_PORT} ]]
-    then
-        echo "Did not find a matching namespace: $1" 1>&2
-        exit 1
-    fi
-    LOCAL_REGISTRY="localhost:${LOCAL_REGISTRY_PORT}/pingcap"
+    local_registry_port=$(rebuild::get_info_by_ns $1 3)
+    local_registry="localhost:${local_registry_port}/pingcap"
 
-    rebuild::step "start push images [${IMAGES}] to  ${LOCAL_REGISTRY} registry with namespace $1"
+    rebuild::step "start push images [${IMAGES}] to  ${local_registry} registry with namespace $1"
     local flag
     for image in ${IMAGES}
     do
@@ -96,11 +103,11 @@ function rebuild::push_images_to_local {
             docker pull ${SOURCE_REGISTRY}/${image} || flag=1
             continue
         done
-        docker tag ${SOURCE_REGISTRY}/${image} ${LOCAL_REGISTRY}/${image}
-        docker push ${LOCAL_REGISTRY}/${image}
+        docker tag ${SOURCE_REGISTRY}/${image} ${local_registry}/${image}
+        docker push ${local_registry}/${image}
     done
 
-    rebuild::step "start push TiDB images [${TIDB_IMAGES}] to ${LOCAL_REGISTRY} registry with namespace $1"
+    rebuild::step "start push TiDB images [${TIDB_IMAGES}] to ${local_registry} registry with namespace $1"
     for image in ${TIDB_IMAGES}
     do
         flag=0
@@ -112,28 +119,47 @@ function rebuild::push_images_to_local {
             docker pull ${SOURCE_REGISTRY}/${image}:${TIDB_BASE_TAG} || flag=1
             continue
         done
-        docker tag ${SOURCE_REGISTRY}/${image}:${TIDB_BASE_TAG} ${LOCAL_REGISTRY}/${image}:${TIDB_BASE_TAG}
-        docker tag ${SOURCE_REGISTRY}/${image}:${TIDB_BASE_TAG} ${LOCAL_REGISTRY}/${image}:master
-        docker push ${LOCAL_REGISTRY}/${image}:${TIDB_BASE_TAG}
-        docker push ${LOCAL_REGISTRY}/${image}:master
+        docker tag ${SOURCE_REGISTRY}/${image}:${TIDB_BASE_TAG} ${local_registry}/${image}:${TIDB_BASE_TAG}
+        docker tag ${SOURCE_REGISTRY}/${image}:${TIDB_BASE_TAG} ${local_registry}/${image}:master
+        docker push ${local_registry}/${image}:${TIDB_BASE_TAG}
+        docker push ${local_registry}/${image}:master
     done
 }
 
-function rebuild::get_ns_info {
-    local -a ns_array
-    local -a cluster_array_info
-    for cluster in ${CLUSTERS[@]}
-    do
-        cluster_array_info=(`echo ${cluster}|awk -F: '{print $1,$2,$3,$4,$5}'`)
-        [[ ${#cluster_array_info[@]} -ne 5 ]] && echo "Wrong cluster info format: ${cluster}, please check it!!!" 1>&2
-
-        ns_array+=(${cluster_array_info[0]})
-    done
-    echo ${ns_array[@]}|tr ' ' '|'
+function rebuild::get_all_ns_info {
+    echo $(rebuild::get_info_by_ns all 0)|tr ' ' '|'
 }
 
-# namespace:dind_subnet:apiserver_port:local_registry_port:cloud_manager_port
-function rebuild::launch_cluster {
+function rebuild::down_cluster {
+    rebuild::step "start to down k8s dind cluster with namespace $1"
+    export DIND_NAMESPACE=$1
+    local kube_version
+    kube_version=`echo ${DIND_NAMESPACE}|awk -F- '{print $NF}'`
+    ./fixed/dind-cluster-${kube_version}.sh down
+}
+
+function rebuild::clean_cluster {
+    rebuild::step "start to clean k8s dind cluster with namespace $1"
+    export DIND_NAMESPACE=$1
+    local kube_version
+    kube_version=`echo ${DIND_NAMESPACE}|awk -F- '{print $NF}'`
+    ./fixed/dind-cluster-${kube_version}.sh clean
+}
+
+# the input args: $namespace $dind_subnet $apiserver_port $local_registry_port $cloud_manager_port
+function rebuild::up_dind {
+    rebuild::step "start to bringing up k8s dind cluster with namespace $1"
+    export DIND_NAMESPACE=$1
+    export DIND_SUBNET=$2
+    export APISERVER_PORT=$3
+    export REGISTRY_PORT=$4
+    export CLOUD_MANAGER_PORT=$5
+    local kube_version
+    kube_version=`echo ${DIND_NAMESPACE}|awk -F- '{print $NF}'`
+    ./fixed/dind-cluster-${kube_version}.sh up
+}
+
+function rebuild::up_cluster {
     local ns=${1:-}
     if [[ ! ${ns} ]]
     then
@@ -162,20 +188,34 @@ function rebuild::launch_cluster {
     done
     if [[ ! ${dind_subnet} ]]
     then
-        echo "The input namespace ${ns} does not match the built-in namespaces `rebuild::get_ns_info`" 1>&2
+        echo "The input namespace ${ns} does not match the built-in namespaces $(rebuild::get_all_ns_info)" 1>&2
         exit 1
     fi
-    rebuild::step "start to launch k8s dind cluster with namespace ${namespace}"
-    rebuild::clean_dind ${namespace}
-    rebuild::clean_images
+    rebuild::step "start to up k8s dind cluster with namespace ${namespace}"
     rebuild::up_dind ${namespace} ${dind_subnet} ${apiserver_port} ${local_registry_port} ${cloud_manager_port}
-    rebuild::start_registry ${namespace}
-    rebuild::deploy_apps ${namespace}
+}
+
+function rebuild::rebuild_cluster {
+    rebuild::step "start to rebuild k8s dind cluster with namespace $1"
+    rebuild::clean_cluster $1
+    rebuild::clean_images
+    rebuild::up_cluster $1
+    rebuild::start_registry $1
+    rebuild::deploy_apps $1
 }
 
 case "${1:-}" in
     rebuild)
-        rebuild::launch_cluster $2
+        rebuild::rebuild_cluster $2
+        ;;
+    clean)
+        rebuild::clean_cluster $2
+        ;;
+    up)
+        rebuild::up_cluster $2
+        ;;
+    down)
+        rebuild::down_cluster $2
         ;;
     push)
         rebuild::push_images_to_local $2
@@ -183,8 +223,11 @@ case "${1:-}" in
     *)
         echo "usage:" 1>&2
         echo "  $0 rebuild namespace" 1>&2
+        echo "  $0 clean namespace" 1>&2
+        echo "  $0 up namespace" 1>&2
+        echo "  $0 down namespace" 1>&2
         echo "  $0 push namespace" 1>&2
-        echo "The possible namespace values are: `rebuild::get_ns_info`" 1>&2
+        echo "The possible namespace values are: $(rebuild::get_all_ns_info)" 1>&2
         exit 1
     ;;
 esac
